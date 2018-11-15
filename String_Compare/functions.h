33fd06a091c7bc356c442dd8cf89f0f56df76b81
Mahmoud Abdelrahman <30775657+MahmoudAbderahman@users.noreply.github.com>
2018-11-15 21:34:22 +0100

    Create functions.h
    A C header file that contains the heads of the methods.

---
+++ String_Compare/functions.h
@@ -0,0 +1,10 @@
+/*
+ * functions.h
+ *
+ *  Created on: 14 Nov 2018
+ *      Author: Mahmoud Abdelrahman
+ */
+void toLower(char * str);
+int stringLength(char * str);
+
+int myStrCmp(const char * str1, const char * str2);
